b6975f07f947a823940e6ad90be5ffd2a7222a12
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2022-12-26 18:18:10 +0800

    Update index.js

--- program/index.js
+++ program/index.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const secret = 'mysecret';
 
-// Set up body parser to parse request bodies
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set up an in-memory "database" of users
+
 const users = {
   'user1': {
     'password': 'password1'
@@ -18,13 +18,13 @@ const users = {
   }
 };
 
-// Set up the login route
+
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Check the username and password against the "database"
+  
   if (username in users && users[username].password === password) {
-    // Create a JWT with a 1-hour expiration time
+    
     const token = jwt.sign({ username }, secret, { expiresIn: '1h' });
     res.send({ token });
   } else {
@@ -32,15 +32,15 @@ app.post('/login', (req, res) => {
   }
 });
 
-// Set up the auth middleware
+
 const authenticate = (req, res, next) => {
-  // Check for the presence of a JWT in the request header
+  
   const authHeader = req.headers.authorization;
   if (authHeader) {
-    // Extract the JWT from the header
+    
     const token = authHeader.split(' ')[1];
     try {
-      // Verify the JWT and attach the decoded payload to the request object
+      
       req.user = jwt.verify(token, secret);
       next();
     } catch (error) {
@@ -51,12 +51,12 @@ const authenticate = (req, res, next) => {
   }
 };
 
-// Set up a protected route
+
 app.get('/protected', authenticate, (req, res) => {
   res.send({ message: `Welcome, ${req.user.username}` });
 });
 
-// Start the server
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
